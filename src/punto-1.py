bac665f5f696aa3cdf051cd477651541103b2f81
Manuel Nuñez <112630074+mnunez-unahur@users.noreply.github.com>
2025-09-30 16:40:56 +0000

    agregada funcionalidad de salvar entrenamiento

--- src/punto-1.py
+++ src/punto-1.py
@@ -18,32 +18,40 @@ utils.set_random_seed(2)
 
 autoencoder1 = models.Sequential()
 
+n = 0.007
+epocas = 5000
+activacion_latente = 'sigmoid'
+
 # autoencoder1.add(layers.Dense(35,activation = 'sigmoid', input_dim=35))
 autoencoder1.add(layers.Input(shape=(35,)))
-autoencoder1.add(layers.Dense(35,activation = 'sigmoid'))
+# autoencoder1.add(layers.Dense(35,activation = 'sigmoid'))
 autoencoder1.add(layers.Dense(17,activation = 'sigmoid'))
-autoencoder1.add(layers.Dense(2, activation = 'sigmoid', name='latente'))
+autoencoder1.add(layers.Dense(2, activation = activacion_latente, name='latente'))
 autoencoder1.add(layers.Dense(17,activation = 'sigmoid'))
 autoencoder1.add(layers.Dense(35,activation = 'sigmoid'))
 
-opt = optimizers.Adam(learning_rate = 0.008)
+
+opt = optimizers.Adam(learning_rate = n)
 metrica = metrics.BinaryAccuracy(name="binary accuracy", dtype=None, threshold=0.5)
 
 # autoencoder1.compile(loss = 'MSE', optimizer = opt, metrics = [metrica])
 autoencoder1.compile(loss = 'binary_crossentropy', optimizer = opt, metrics = [metrica])
 
-historia = autoencoder1.fit(X, X, epochs = 3500, batch_size = 1)
+historia = autoencoder1.fit(X, X, epochs = epocas, batch_size = 1)
 
 res = a_bit(autoencoder1.predict(X))
 
+cantErrores = 0
 for i in range(len(X)):
     if not np.array_equal(X[i], res[i]):
         print("--------------------------------------   ")
         print (X[i])
         print(res[i])
+        cantErrores += 1
 
-capa_latente = autoencoder1.layers[3]
-print(capa_latente.name)
+print(f"porcentaje de error: {cantErrores * 100 / len(X)}%")
+if cantErrores == 0:
+    autoencoder1.save(str(n)+"_"+str(epocas)+"_"+activacion_latente+".keras")
 
 
 
