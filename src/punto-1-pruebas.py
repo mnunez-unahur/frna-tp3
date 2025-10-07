fec954ae5260aead4831ee006911c8c00a5da5e4
Manuel Nuñez <112630074+mnunez-unahur@users.noreply.github.com>
2025-10-07 12:15:30 +0000

    agregadas pruebas con otras fuentes

--- src/punto-1-pruebas.py
+++ src/punto-1-pruebas.py
@@ -1,58 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from datos import font1
+from datos import font1, font2, font3
 from util import hexa_a_matriz, a_bit
 from keras import models, layers, optimizers, metrics, utils, activations
-
-X = []
-for d in font1:
-    # print(f"convirtiendo {d} a matriz")
-    m = hexa_a_matriz(d).reshape(35)
-    # print(m)
-    X.append(m)
-X = np.array(X)
-
 
 codificador=models.load_model("punto-1-codificador.keras")
 decodificador=models.load_model("punto-1-decodificador.keras")
 autoencoder1 = models.Sequential([codificador,decodificador])
 
+
+X = []
+font = "font2"
+for d in font2:
+    m = hexa_a_matriz(d).reshape(35)
+    X.append(m)
+X = np.array(X)
+
 res = a_bit(autoencoder1.predict(X))
 
 cantErrores = 0
 for i in range(len(X)):
+    x = X[i].reshape(7,5)
+    y_esperada = X[i].reshape(7,5)
+    y_obtenida = res[i].reshape(7,5)
     if not np.array_equal(X[i], res[i]):
-        print("--------------------------------------   ")
-        print (X[i])
-        print(res[i])
         cantErrores += 1
+        resultado = "Falla"
+
+        plt.clf()
+        plt.imshow(x,interpolation="nearest")
+        plt.savefig(f"punto-1-{font}-{i}-{resultado}-x.png")
+        
+        plt.clf()
+        plt.imshow(y_esperada,interpolation="nearest")
+        plt.savefig(f"punto-1-{font}-{i}-{resultado}-y_esperada.png")
+        
+        plt.clf()
+        plt.imshow(y_obtenida,interpolation="nearest")
+        plt.savefig(f"punto-1-{font}-{i}-{resultado}-y_obtenida_{i}.png")
 
 
 print(f"porcentaje de error: {cantErrores * 100 / len(X)}%")
 
+
 resultado_latente=codificador.predict(X)
 
-print(resultado_latente)
 x_latente = [p[0] for p in resultado_latente]
 y_latente = [p[1] for p in resultado_latente]
 
+plt.clf() # limpio la imagen
 plt.figure(1)
 plt.plot(resultado_latente)
 plt.title("predict: latente")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.legend("legend",loc="upper right")
+plt.legend("capa latente",loc="upper right")
 plt.figure(figsize=(8,6))
 plt.scatter(x_latente, y_latente)
-plt.savefig("punto-1-predict-latente.png")
+plt.savefig(f"punto-1-{font}-predict-latente.png")
 plt.clf() # limpio la imagen
 
-#result = a_bit(decodificador.predict(np.array([[2.66707186e-02, 1.21699385e-01]])))
-result = a_bit(decodificador.predict(np.array([[0.54632, 0.57643]])))
-print(result[0].reshape(7,5))
-
-plt.figure(2)
-plt.imshow(result[0].reshape(7,5),interpolation="nearest")
-plt.savefig("punto-1-predict-resultado.png")
 
--- src/punto-2-pruebas.py
+++ src/punto-2-pruebas.py
@@ -15,7 +15,7 @@ for d in font1:
     Y.append(m)
     # agregamos versiones distorsionadas
     for i in range(10):
-        con_ruido = distorsionar(m, 0.1)
+        con_ruido = distorsionar(m, 0.3)
         X.append(con_ruido)
         Y.append(m)
         if not np.array_equal(m, con_ruido):
@@ -27,8 +27,8 @@ for d in font1:
 X = np.array(X)
 Y = np.array(Y)
 
-codificador=models.load_model("punto-2-codificador.keras")
-decodificador=models.load_model("punto-2-decodificador.keras")
+codificador=models.load_model("punto-1-codificador.keras")
+decodificador=models.load_model("punto-1-decodificador.keras")
 autoencoder1 = models.Sequential([codificador,decodificador])
 
 #res = a_bit(autoencoder1.predict(X))
@@ -42,7 +42,6 @@ for i in range(len(X)):
     y_obtenida = res[i].reshape(7,5)
     if not np.array_equal(Y[i], a_bit(res)[i]):
         resultado = "Falla"
-        # print("--------------------------------------   ")
         cantErrores += 1
 
         #    if resultado == "Ok":
@@ -50,14 +49,31 @@ for i in range(len(X)):
         plt.imshow(x,interpolation="nearest")
         plt.savefig(f"punto-2-{i}-{resultado}-x.png")
         
-        # plt.clf()
-        # plt.imshow(y_esperada,interpolation="nearest")
-        # plt.savefig(f"punto-2-{i}-{resultado}-y_esperada.png")
+        plt.clf()
+        plt.imshow(y_esperada,interpolation="nearest")
+        plt.savefig(f"punto-2-{i}-{resultado}-y_esperada.png")
         
         plt.clf()
         plt.imshow(y_obtenida,interpolation="nearest")
         plt.savefig(f"punto-2-{i}-{resultado}-y_obtenida_{i}.png")
 
+
+resultado_latente=codificador.predict(X)
+
+x_latente = [p[0] for p in resultado_latente]
+y_latente = [p[1] for p in resultado_latente]
+
+plt.clf() # limpio la imagen
+plt.figure(1)
+plt.plot(resultado_latente)
+plt.title("predict: latente")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend("capa latente",loc="upper right")
+plt.figure(figsize=(8,6))
+plt.scatter(x_latente, y_latente)
+plt.savefig(f"punto-2-predict-latente.png")
+plt.clf() # limpio la imagen
 
 print(f"cantidad de muestras con ruido: {cantidad_diferentes}/{len(X)}:: {cantidad_diferentes/len(X)*100}%")
 print(f"porcentaje de error: {cantErrores * 100 / len(X)}%")
